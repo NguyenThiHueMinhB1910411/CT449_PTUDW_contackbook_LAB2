2f6b3e835c2038b6bdc5a7f303667f14b2de9ba3
huongwisme <minhb1910411@student.ctu.edu.vn>
2022-09-22 13:04:31 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
-const express = require("express");
-const cors = require("cors");
 
-const app = express();
+const express = require("express");
+const ApiError = require("./app/api-error");
+
 const contactsRouter = require("./app/routers/contact.route");
+
+const cors = require("cors");
+const app = express();
 
 app.use(cors());
 app.use("/api/contacts", contactsRouter);
-app.use(express.json());
-
+app.use((req, res, next)=>{
+    return next(new ApiError(404, "Resource not found"));
+});
+//define error
+app.use((err,req,res,next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
+    });
+});
 app.get("/",(req, res) => {
     res.json({message:" Welcome to contact book application."});
 });
